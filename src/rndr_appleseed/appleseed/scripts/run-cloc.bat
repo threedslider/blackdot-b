87375912640a5b22f96f2ec6018cfbfc856bc4ad
Laurent LE FLEM <tecz.cg@gmail.com>
2023-08-04 08:26:00 +0200

    removed to appleseed and luxcorerender due for file size

--- src/rndr_appleseed/appleseed/scripts/run-cloc.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-tools\cloc\cloc-1.50.exe --list-file=cloclist.txt --force-lang=C,osl
-
-pause
--- src/rndr_luxcorerender/luxcorerender/scripts/azurepipeline/windows/build.cmd
+++
@@ -1,4 +0,0 @@
-cd ..\WindowsCompile
-call .\cmake-build-x64.bat %1 %2 %3 %4
-if ERRORLEVEL 1 exit /B 1
-cd ..
